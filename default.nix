6a4428ac0bfa644bba4264dfffeadc21390fc5d2
DJP <djpade@gmail.com>
2017-12-01 22:17:49 -0500

    First commit

---
+++ default.nix
@@ -0,0 +1,12 @@
+#{ mkDerivation, base, protolude, set-monad, stdenv }:
+{pkgs ? import <nixpkgs> {}}:
+
+pkgs.haskellPackages.mkDerivation {
+  pname = "padelude";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = with pkgs.haskellPackages; [ base protolude set-monad ];
+  homepage = "https://github.com/qfjp/padelude";
+  description = "A reasonable prelude";
+  license = with pkgs; stdenv.lib.licenses.gpl3;
+}

0b8b5db842f26c1c8988d6f544b91ede07be92b0
DJP <djpade@gmail.com>
2018-02-21 17:58:52 -0500

    Add printf

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ pkgs.haskellPackages.mkDerivation {
   pname = "padelude";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = with pkgs.haskellPackages; [ base protolude set-monad ];
+  libraryHaskellDepends = with pkgs.haskellPackages; [ base protolude set-monad text ];
   homepage = "https://github.com/qfjp/padelude";
   description = "A reasonable prelude";
   license = with pkgs; stdenv.lib.licenses.gpl3;
--- src/Padelude.hs
+++ src/Padelude.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
-module Padelude (module P, module Set, (++)) where
+module Padelude (module P, module Set, (++), printf) where
 
 import           Data.Semigroup (Semigroup, (<>))
 import           Data.Set.Monad as Set (Set)
 import           Protolude      as P hiding (Set, on, (++), (<>))
+import           Text.Printf    (printf)
 
 infixr 5  ++
 (++) :: Semigroup a => a -> a -> a

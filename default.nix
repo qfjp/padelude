8d6a3f0e96a6ab8ec344b8a8c64c8f391ed5ef2f
DJP <djpade@gmail.com>
2018-09-17 11:23:04 -0400

    Fix for 8.4.3 (Semigroup/Monoid Superclass Merge)

--- default.nix
+++ default.nix
@@ -1,12 +1,21 @@
 #{ mkDerivation, base, protolude, set-monad, stdenv }:
 {pkgs ? import <nixpkgs> {}}:
 
-pkgs.haskellPackages.mkDerivation {
-  pname = "padelude";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = with pkgs.haskellPackages; [ base protolude set-monad text ];
-  homepage = "https://github.com/qfjp/padelude";
-  description = "A reasonable prelude";
-  license = with pkgs; stdenv.lib.licenses.gpl3;
+
+let set-monad = pkgs.haskellPackages.callCabal2nix "set-monad"
+  (pkgs.fetchFromGitHub {
+    owner = "giorgidze";
+    repo = "set-monad";
+    rev = "86f7130ee9bfcd95c4801cbd9f6a6e886a42c160";
+    sha256 = "14dy9vhqfwdh3xb0z8ggdqnrfpggixq7yysp2n12s1p5wsd8pwgw";
+  }) {};
+in
+  pkgs.haskellPackages.mkDerivation {
+    pname = "padelude";
+    version = "0.1.2.2";
+    src = ./.;
+    libraryHaskellDepends = with pkgs.haskellPackages; [ base protolude set-monad text ];
+    homepage = "https://github.com/qfjp/padelude";
+    description = "A reasonable prelude";
+    license = with pkgs; stdenv.lib.licenses.gpl3;
 }
